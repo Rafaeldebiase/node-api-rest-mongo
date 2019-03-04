099b5b8b5fe5c6c87236284b414163b8c5c6ff88
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-03-03 23:40:27 -0300

    cria rota para o post

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (required, response, next) => {
+const route = router.get('/', (request, response, next) => {
     response.status(200).send({
         title: "Node Store API",
         verison: "0.0.1"
     })
 });
+
+const create = router.post('/', (request, response, next) => {
+    response.status(201).send(request.body);
+});
+
+
+app.use('/products', create);
 app.use('/', route);
 
 module.exports = app;
