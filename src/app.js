802208b50afd438cc73a2a7b114c8d8c61a0689c
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-03-04 00:00:55 -0300

    cria metodos put e delete

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 'use strict'
 
 const express = require('express');
-const bodyparse = require( 'body-parser');
+const bodyparse = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
 app.use(bodyparse.json());
-app.use(bodyparse.urlencoded({ extended: false }));
+app.use(bodyparse.urlencoded({
+    extended: false
+}));
 
 const route = router.get('/', (request, response, next) => {
     response.status(200).send({
@@ -20,9 +22,23 @@ const create = router.post('/', (request, response, next) => {
     response.status(201).send(request.body);
 });
 
+const put = router.put('/:id', (request, response, next) => {
+    const id = request.params.id;
+    response.status(200).send({
+        id: id,
+        item: request.body
+    });
+});
+
+const del = router.delete('/', (request, response, next) => {
+    response.status(200).send(request.body);
+});
+
+
 
 app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 app.use('/', route);
 
 module.exports = app;
-
