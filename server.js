65593b1f5d98de2c1ac16ed366d896e61398f7c1
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-03-03 22:46:42 -0300

    server: normalizePort - nomaliza a porta utilizada pelo servidor

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -19,3 +19,16 @@ const route = router.get('/', (required, response, next) => {
 });
 app.use('/', route);
 
+server.listen(port);
+console.log('API rodando na porta ' + port);
+
+function normalizePort(value) {
+    const port = parseInt(value, 10);
+    if (isNaN(port)) {
+        return value;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false
+}
