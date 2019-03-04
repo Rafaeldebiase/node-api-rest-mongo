b57b6b34ee29b429cce95d076013ede67997d561
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-03-04 00:14:55 -0300

    refatora appe cria rotas crud

--- src/app.js
+++ src/app.js
@@ -4,41 +4,21 @@ const express = require('express');
 const bodyparse = require('body-parser');
 
 const app = express();
-const router = express.Router();
+
+const index = require('./routes/index');
+const product = require('./routes/product');
+
 
 app.use(bodyparse.json());
 app.use(bodyparse.urlencoded({
     extended: false
 }));
 
-const route = router.get('/', (request, response, next) => {
-    response.status(200).send({
-        title: "Node Store API",
-        verison: "0.0.1"
-    })
-});
-
-const create = router.post('/', (request, response, next) => {
-    response.status(201).send(request.body);
-});
-
-const put = router.put('/:id', (request, response, next) => {
-    const id = request.params.id;
-    response.status(200).send({
-        id: id,
-        item: request.body
-    });
-});
-
-const del = router.delete('/', (request, response, next) => {
-    response.status(200).send(request.body);
-});
 
 
 
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
-app.use('/', route);
+
+app.use('/products', product);
+app.use('/', index);
 
 module.exports = app;
