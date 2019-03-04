5b4f9f57d738c7c5e1485b60a2d8526f7216e7ce
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-03-03 22:57:28 -0300

    server: onError

--- server.js
+++ server.js
@@ -32,3 +32,27 @@ function normalizePort(value) {
     }
     return false
 }
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'pipe' + port :
+        'port' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+    
+}
