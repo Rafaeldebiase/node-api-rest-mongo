3628d53dbe69fa0822812c568770b3985c297e78
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-03-04 00:31:03 -0300

    refatora rotas e cria controller

--- src/app.js
+++ src/app.js
@@ -8,15 +8,10 @@ const app = express();
 const index = require('./routes/index');
 const product = require('./routes/product');
 
-
 app.use(bodyparse.json());
 app.use(bodyparse.urlencoded({
     extended: false
 }));
-
-
-
-
 
 app.use('/products', product);
 app.use('/', index);
