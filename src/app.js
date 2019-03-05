a849c3bcc31b5a8464985549cf9f8ed78ca4f21e
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-03-05 20:52:28 -0300

    insere a conection string do banco

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const bodyparse = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://rafael:omerta12-.@mongostr-yd1ew.gcp.mongodb.net/test?retryWrites=true')
 
 const index = require('./routes/index');
 const product = require('./routes/product');
