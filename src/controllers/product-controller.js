71c09bc12f139577a556af72972f70d06178785a
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-03-05 23:36:32 -0300

    insere metodos get e melhorias no crud

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,15 +1,91 @@
 'use strict'
 
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+exports.get = (request, response, next) => {
+    Product.find({
+        active: true
+    }, 'title price slug').then((result) => {
+        response.status(200).send(result);
+    }).catch((err) => {
+        response.status(400).send(err);
+    });
+};
+
+exports.getById = (request, response, next) => {
+    Product.findById(request.params.id).then((result) => {
+        response.status(200).send(result);
+    }).catch((err) => {
+        response.status(400).send(err);
+    });
+};
+
+exports.getBySlug = (request, response, next) => {
+    Product.findOne({
+        slug: request.params.slug,
+        active: true
+    }, 'title description price slug tags').then((result) => {
+        response.status(200).send(result);
+    }).catch((err) => {
+        response.status(400).send(err);
+    });
+};
+
+exports.getByTag = (request, response, next) => {
+    Product.find({
+        tags: request.params.tag,
+        active: true
+    }, 'title description price slug tags').then((result) => {
+        response.status(200).send(result);
+    }).catch((err) => {
+        response.status(400).send(err);
+    });
+};
+
 exports.post = (request, response, next) => {
-    response.status(201).send(request.body);
+    let product = new Product(request.body);
+    product.save().then((result) => {
+        response.status(201).send({
+            message: 'Produto cadastrado com sucesso',
+            data: result
+        });
+    }).catch((err) => {
+        response.status(400).send({
+            message: 'Falha ao cadastrar produto',
+            data: err
+        });
+    });
 };
 exports.put = (request, response, next) => {
-    const id = request.params.id;
-    response.status(200).send({
-        id: id,
-        item: request.body
+    Product.findByIdAndUpdate(request.params.id, {
+        $set: {
+            title: request.body.title,
+            description: request.body.description,
+            slug: request.body.slug,
+            price: request.body.price
+        }
+    }).then((result) => {
+        response.status(200).send({
+            message: 'Produto atualizado com sucesso'
+        });
+    }).catch((err) => {
+        response.status(400).send({
+            message: 'Falha ao atualizar o produto',
+            data: err
+        });
     });
 };
 exports.delete = (request, response, next) => {
-    response.status(200).send(request.body);
+    Product.findOneAndRemove(request.body.id)
+    .then((result) => {
+        response.status(200).send({
+            message: 'Produto removido com sucesso'
+        });
+    }).catch((err) => {
+        response.status(400).send({
+            message: 'Falha ao remover o produto',
+            data: err
+        });
+    });
 };
